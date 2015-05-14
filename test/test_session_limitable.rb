c30e072948488ee95ee63d3931c1b1c154b3bdfe
Tomas Valent <equivalent@eq8.eu>
2015-05-14 20:47:50 +0100

    save work

--- test/test_session_limitable.rb
+++ test/test_session_limitable.rb
@@ -1,27 +1,60 @@
 require 'test_helper'
 
 class SessionLimitableEvaluator
+  RecordDBColumnsMisconfiguration = Class.new(StandardError)
 
   attr_reader :record
-  attr_accessor :unique_session_id
+  attr_accessor :unique_session_id, :user_agent, :remote_ip
 
   def initialize(record)
     @record = record
   end
 
   def evaluate_sign_in
-    if session_limitable_on_unique_id?
+    if record_uses_session_limitable_module?
+      ensure_field_existance
       yield
     end
   end
 
   def evaluate_unauthorized_access
-    if session_limitable_on_unique_id? && record.unique_session_id != unique_session_id
-      yield
+    if record_uses_session_limitable_module?
+      ensure_field_existance
+      if restricted_by_unique_session_id? || restricted_by_ip? || restricted_by_user_agent?
+        yield
+      end
     end
   end
 
   private
+
+  def restricted_by_unique_session_id?
+    RestrictedBySubmodule
+      .new(session_limitable_on_unique_id?)
+      .call { record.unique_session_id != unique_session_id }
+  end
+
+  def restricted_by_ip?
+    RestrictedBySubmodule
+      .new(session_limitable_on_unique_id?)
+      .call { record.unique_session_id != unique_session_id }
+  end
+
+  def restricted_by_user_agent?
+    RestrictedBySubmodule
+      .new(session_limitable_on_unique_id?)
+      .call { record.unique_session_id != unique_session_id }
+  end
+
+  RestrictedBySubmodule = Struct.new(:use_submodule) do
+    def call
+      if use_submodule
+        yield
+      else
+        false
+      end
+    end
+  end
 
   # When record includes module SessionLimitable
   #
@@ -34,12 +67,43 @@ class SessionLimitableEvaluator
     record.respond_to?(:update_unique_session_id!)
   end
 
-  def session_limitable_on_unique_id?
-    Devise.session_limitable_on_unique_id \
-      && record_uses_session_limitable_module?
+  def ensure_field_existance
+    if session_limitable_on_unique_id? && !record.respond_to?(:unique_session_id)
+      raise RecordDBColumnsMisconfiguration, 'unique_session_id column is missing'
+    end
   end
 
-  #RecordDBColumnsMisconfiguration = Class.new(StandardError)
+  def session_limitable_on_unique_id?
+    Devise.session_limitable_on_unique_id
+  end
+
+  def session_limitable_on_ip?
+    Devise.session_limitable_on_ip
+  end
+
+  def session_limitable_on_user_agent?
+    Devise.session_limitable_on_user_agent
+  end
+
+  #if warden.authenticated?(scope) \
+      #&& options[:store] != false \
+      #&& record.respond_to?(:current_user_agent) \
+      #&& record.respond_to?(:current_sign_in_ip)
+
+    #if record.current_user_agent != warden.request.user_agent || record.current_sign_in_ip != warden.request.remote_ip
+      #warden.logout(scope)
+      #throw :warden, :scope => scope, :message => :session_non_transferable
+    #end
+  #end
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+
   #
   #def validate_record_database_integrity
     #unless session_limitable_on_unique_id? && record.respond_to?(:unique_session_id)
@@ -50,6 +114,11 @@ end
 
 
 class TestSessionLimitableLogic < ActiveSupport::TestCase
+  Struct.new('MisconfiguredSessionLimitableRecord') do
+    def update_unique_session_id!
+    end
+  end
+
   Struct.new('RecordUsingSessionLimitable') do
     def update_unique_session_id!
     end
@@ -60,7 +129,7 @@ class TestSessionLimitableLogic < ActiveSupport::TestCase
     end
   end
 
-  Struct.new('RecordUsingNotUsingSessionLimitable') do
+  Struct.new('RecordNotUsingSessionLimitable') do
 
     # this represents value in DB column
     def unique_session_id
@@ -87,11 +156,31 @@ class TestSessionLimitableLogic < ActiveSupport::TestCase
   end
 
   test 'evaluate_sign_in should not call block when record is not using SessionLimitable module' do
-    record = Struct::RecordUsingNotUsingSessionLimitable.new
+    record = Struct::RecordNotUsingSessionLimitable.new
     evaluator = described_class.new(record)
 
     evaluator.evaluate_sign_in do
       @spy_result = 'called'
+    end
+
+    assert_equal(@spy_result, 'not-called')
+  end
+
+  test 'evaluate_sign_in should raise error when record uses SessionLimitable module but have not required DB columns' do
+    record = Struct::MisconfiguredSessionLimitableRecord.new
+    evaluator = described_class.new(record)
+
+    assert_raise(SessionLimitableEvaluator::RecordDBColumnsMisconfiguration) do
+      evaluator.evaluate_sign_in {}
+    end
+  end
+
+  test 'evaluate_unauthorized_access should not call block when not using SessionLimitable module' do
+    record = Struct::RecordNotUsingSessionLimitable.new
+    evaluator = described_class.new(record)
+
+    evaluator.evaluate_unauthorized_access do
+      @spy_result = 'loggout called'
     end
 
     assert_equal(@spy_result, 'not-called')
@@ -103,22 +192,22 @@ class TestSessionLimitableLogic < ActiveSupport::TestCase
     evaluator.unique_session_id = 'valid-session-id'
 
     evaluator.evaluate_unauthorized_access do
-      @spy_result = 'called'
+      @spy_result = 'loggout called'
     end
 
     assert_equal(@spy_result, 'not-called')
   end
 
-  test 'evaluate_unauthorized_access should  call block when record when unique session id dont match' do
+  test "evaluate_unauthorized_access should call block when record when unique session id don't match" do
     record = Struct::RecordUsingSessionLimitable.new
     evaluator = described_class.new(record)
     evaluator.unique_session_id = 'invalid-session-id'
 
     evaluator.evaluate_unauthorized_access do
-      @spy_result = 'called'
+      @spy_result = 'loggout called'
     end
 
-    assert_equal(@spy_result, 'called')
+    assert_equal(@spy_result, 'loggout called')
   end
 
 
